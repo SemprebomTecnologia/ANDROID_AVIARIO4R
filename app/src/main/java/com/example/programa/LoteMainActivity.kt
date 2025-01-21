cb6c854b154dd076b753528912b1ee4baa979882
matheusfreire.ti@hotmail.com <Maria@281016>
2025-01-21 12:06:11 -0300

    AVIARIO COMMIT 21/05/2025
    COMMIT

--- app/src/main/java/com/example/programa/LoteMainActivity.kt
+++ app/src/main/java/com/example/programa/LoteMainActivity.kt
@@ -4,15 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.programa.databinding.ActivityColetaMainBinding
+import com.example.programa.databinding.ActivityLoteMainBinding
 import com.example.programa.databinding.ActivityPrincipalMainBinding
 
-class Coleta_MainActivity : AppCompatActivity() {
+class LoteMainActivity : AppCompatActivity() {
 
     private  lateinit  var MainPrincipal  : ActivityPrincipalMainBinding
-    private  lateinit  var MainColeta     : ActivityColetaMainBinding
+    private  lateinit  var MainColeta     : ActivityLoteMainBinding
 
     //public  lateinit  var MainPrincipal  : ActivityPrincipalMainBinding
     //private lateinit  var MainColeta     : ActivityColetaMainBinding
@@ -22,7 +20,7 @@ class Coleta_MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
 
         //vamos inflar este  layout
-        MainColeta = ActivityColetaMainBinding.inflate(layoutInflater)
+        MainColeta = ActivityLoteMainBinding.inflate(layoutInflater)
         //inflo o layout
         setContentView(MainColeta.root)
         /*
--- app/src/main/java/com/example/programa/PrincipalMainActivity.kt
+++ app/src/main/java/com/example/programa/PrincipalMainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.programa
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.programa.databinding.ActivityColetaMainBinding
 import com.example.programa.databinding.ActivityPrincipalMainBinding
 
 class PrincipalMainActivity : AppCompatActivity() {
@@ -36,7 +31,7 @@ class PrincipalMainActivity : AppCompatActivity() {
         //Programando o evento do botao
         MainPrincipal.btnCHEGADA.setOnClickListener(){
             //informo a tela
-            val MudarTela = Intent(this,Coleta_MainActivity::class.java)
+            val MudarTela = Intent(this,LoteMainActivity::class.java)
             //mostro a atividade
             startActivity(MudarTela)
         }
